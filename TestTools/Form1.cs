768c909700b954dccb4f7a832914f4704d175c7b
sxycxwb@163.com <sxycxwb@163.com>
2017-07-14 02:11:40 +0800

    提交代码

--- TestTools/Form1.cs
+++ TestTools/Form1.cs
@@ -6,7 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
-using Spire.Doc;
+//using Spire.Doc;
 using UtilityCode;
 
 namespace TestTools
@@ -20,20 +20,21 @@ namespace TestTools
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Document document = new Document();
-            document.LoadFromFile(@"1.doc");
-            var section = document.Sections[0].Body;
-            var a = section.Document;
+            //Document document = new Document();
+            //document.LoadFromFile(@"1.doc");
+            //var section = document.Sections[0].Body;
+            //var a = section.Document;
 
-            //段落
-            Spire.Doc.Collections.ParagraphCollection p  = document.Sections[0].Tables[0].Rows[12].Cells[0].Paragraphs;
-            string c = p[0].Text;
+            ////段落
+            //Spire.Doc.Collections.ParagraphCollection p  = document.Sections[0].Tables[0].Rows[12].Cells[0].Paragraphs;
+            //string c = p[0].Text;
             //var doc = new WordTableRead("1.doc");
             //doc.Open();
             //string b = doc.ReadWord(0, 1, 1);
             //doc.Close();
 
             PdfUtility.ConvertExcel2PDF("1.xlsx","1.pdf");
+            PdfUtility.ConvertExcel2PDF("2.xlsx", "2.pdf");
         }
     }
 }
